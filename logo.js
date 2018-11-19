7d4da618d897be5768b2224ff5f0b0e1240d470a
Chris Masters <chrismasters@gmail.com>
2018-11-19 18:59:54 +0100

    Working with font

--- logo.js
+++ logo.js
@@ -4,10 +4,24 @@ const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
-const sketch = () => {
+const sketch = async () => {
+  const font = new window.FontFace('IBMPlexMono-Regular', 'url(assets/fonts/IBMPlexMono-Regular.woff2)');
+
+  await font.load();
+  document.fonts.add(font);
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    const x = width / 2;
+    const y = height / 2;
+
+    context.fillStyle = 'black';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.font = `200px "IBMPlexMono-Regular"`;
+    context.fillText('SuperCollider', x, y + 400);
   };
 };
 
